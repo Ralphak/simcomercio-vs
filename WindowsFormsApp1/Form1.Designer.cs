2105cb3a3ee36a92ee3a52a3ea380186adb90ef2
Rafael Alves <ralphawk@gmail.com>
2018-08-20 18:18:29 -0300

    novo banco de dados

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -32,21 +32,10 @@ namespace WindowsFormsApp1
         {
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.ListaProdutos = new System.Windows.Forms.ListView();
-            this.ID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.Produto = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.Tipo = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.NCM = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Modal = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.Qtd = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.Preço = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.listView1 = new System.Windows.Forms.ListView();
-            this.ID2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.Produto2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.Tipo2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.Qtd2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.Preço2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button5 = new System.Windows.Forms.Button();
-            this.button1 = new System.Windows.Forms.Button();
             this.AddProduto = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
@@ -54,10 +43,9 @@ namespace WindowsFormsApp1
             // pictureBox1
             // 
             this.pictureBox1.Image = global::WindowsFormsApp1.Properties.Resources.mercosul;
-            this.pictureBox1.Location = new System.Drawing.Point(13, 13);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.pictureBox1.Location = new System.Drawing.Point(10, 11);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(420, 527);
+            this.pictureBox1.Size = new System.Drawing.Size(315, 428);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
@@ -65,131 +53,45 @@ namespace WindowsFormsApp1
             // ListaProdutos
             // 
             this.ListaProdutos.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.ID,
-            this.Produto,
-            this.Tipo,
+            this.NCM,
+            this.Modal,
             this.Qtd,
             this.Preço});
             this.ListaProdutos.FullRowSelect = true;
-            this.ListaProdutos.Location = new System.Drawing.Point(441, 13);
-            this.ListaProdutos.Margin = new System.Windows.Forms.Padding(4);
+            this.ListaProdutos.Location = new System.Drawing.Point(331, 11);
             this.ListaProdutos.Name = "ListaProdutos";
-            this.ListaProdutos.Size = new System.Drawing.Size(420, 527);
+            this.ListaProdutos.Size = new System.Drawing.Size(300, 430);
             this.ListaProdutos.TabIndex = 1;
             this.ListaProdutos.UseCompatibleStateImageBehavior = false;
             this.ListaProdutos.View = System.Windows.Forms.View.Details;
             this.ListaProdutos.MouseDoubleClick += new System.Windows.Forms.MouseEventHandler(this.ListaProdutos_MouseDoubleClick);
             // 
-            // ID
+            // NCM
             // 
-            this.ID.Text = "ID";
-            this.ID.Width = 40;
+            this.NCM.Text = "NCM";
+            this.NCM.Width = 70;
             // 
-            // Produto
+            // Modal
             // 
-            this.Produto.Text = "Produto";
-            this.Produto.Width = 90;
-            // 
-            // Tipo
-            // 
-            this.Tipo.Text = "Tipo";
+            this.Modal.Text = "Modal";
+            this.Modal.Width = 90;
             // 
             // Qtd
             // 
             this.Qtd.Text = "Qtd";
-            this.Qtd.Width = 40;
+            this.Qtd.Width = 50;
             // 
             // Preço
             // 
             this.Preço.Text = "Preço";
             this.Preço.Width = 80;
             // 
-            // listView1
-            // 
-            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
-            this.ID2,
-            this.Produto2,
-            this.Tipo2,
-            this.Qtd2,
-            this.Preço2});
-            this.listView1.FullRowSelect = true;
-            this.listView1.Location = new System.Drawing.Point(977, 13);
-            this.listView1.Margin = new System.Windows.Forms.Padding(4);
-            this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(420, 527);
-            this.listView1.TabIndex = 2;
-            this.listView1.UseCompatibleStateImageBehavior = false;
-            this.listView1.View = System.Windows.Forms.View.Details;
-            // 
-            // ID2
-            // 
-            this.ID2.Text = "ID";
-            this.ID2.Width = 40;
-            // 
-            // Produto2
-            // 
-            this.Produto2.Text = "Produto";
-            this.Produto2.Width = 90;
-            // 
-            // Tipo2
-            // 
-            this.Tipo2.Text = "Tipo";
-            // 
-            // Qtd2
-            // 
-            this.Qtd2.Text = "Qtd";
-            this.Qtd2.Width = 40;
-            // 
-            // Preço2
-            // 
-            this.Preço2.Text = "Preço";
-            this.Preço2.Width = 80;
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(870, 188);
-            this.button2.Margin = new System.Windows.Forms.Padding(4);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(100, 28);
-            this.button2.TabIndex = 4;
-            this.button2.Text = ">";
-            this.button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            this.button3.Location = new System.Drawing.Point(870, 224);
-            this.button3.Margin = new System.Windows.Forms.Padding(4);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(100, 28);
-            this.button3.TabIndex = 5;
-            this.button3.Text = "<";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // button5
-            // 
-            this.button5.Location = new System.Drawing.Point(870, 487);
-            this.button5.Margin = new System.Windows.Forms.Padding(4);
-            this.button5.Name = "button5";
-            this.button5.Size = new System.Drawing.Size(100, 28);
-            this.button5.TabIndex = 7;
-            this.button5.Text = "Limpar";
-            this.button5.UseVisualStyleBackColor = true;
-            // 
-            // button1
-            // 
-            this.button1.Location = new System.Drawing.Point(870, 441);
-            this.button1.Margin = new System.Windows.Forms.Padding(4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(100, 28);
-            this.button1.TabIndex = 8;
-            this.button1.Text = "Finalizar";
-            this.button1.UseVisualStyleBackColor = true;
-            // 
             // AddProduto
             // 
-            this.AddProduto.Location = new System.Drawing.Point(870, 30);
+            this.AddProduto.Location = new System.Drawing.Point(636, 11);
+            this.AddProduto.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.AddProduto.Name = "AddProduto";
-            this.AddProduto.Size = new System.Drawing.Size(100, 50);
+            this.AddProduto.Size = new System.Drawing.Size(75, 41);
             this.AddProduto.TabIndex = 9;
             this.AddProduto.Text = "Adicionar Produto";
             this.AddProduto.UseVisualStyleBackColor = true;
@@ -197,19 +99,13 @@ namespace WindowsFormsApp1
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1412, 553);
+            this.ClientSize = new System.Drawing.Size(721, 449);
             this.Controls.Add(this.AddProduto);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.button5);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.listView1);
             this.Controls.Add(this.ListaProdutos);
             this.Controls.Add(this.pictureBox1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4);
             this.MaximizeBox = false;
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
@@ -224,21 +120,10 @@ namespace WindowsFormsApp1
 
         private PictureBox pictureBox1;
         private ListView ListaProdutos;
-        private ColumnHeader Produto;
+        private ColumnHeader Modal;
         private ColumnHeader Preço;
-        private ColumnHeader Tipo;
-        private ListView listView1;
-        private ColumnHeader Produto2;
-        private ColumnHeader Preço2;
-        private ColumnHeader Tipo2;
-        private Button button2;
-        private Button button3;
-        private Button button5;
-        private Button button1;
-        private ColumnHeader ID;
+        private ColumnHeader NCM;
         private ColumnHeader Qtd;
-        private ColumnHeader ID2;
-        private ColumnHeader Qtd2;
         private Button AddProduto;
     }
 }
--- WindowsFormsApp1/Form1.cs
+++ WindowsFormsApp1/Form1.cs
@@ -24,9 +24,9 @@ namespace WindowsFormsApp1
             {
                 ListViewItem item = new ListViewItem(reader.GetInt32(0).ToString());
                 item.SubItems.Add(reader.GetString(1));
-                item.SubItems.Add(reader.GetString(2));
-                item.SubItems.Add(reader.GetInt32(4).ToString());
-                item.SubItems.Add("R$" + reader.GetSqlMoney(5));
+                item.SubItems.Add(reader.GetDouble(28).ToString());
+                item.SubItems.Add(reader.GetDouble(30).ToString());
+                
                 ListaProdutos.Items.Add(item);
             }
             conexaoDB.Close();
@@ -34,20 +34,18 @@ namespace WindowsFormsApp1
 
         private void ListaProdutos_MouseDoubleClick(object sender, MouseEventArgs e)
         {
-            cmdDB.CommandText = "select * from Produtos where Id=" + ListaProdutos.SelectedItems[0].Text;
+            cmdDB.CommandText = "select * from Produtos where NCM=" + ListaProdutos.SelectedItems[0].Text;
             conexaoDB.Open();
             reader = cmdDB.ExecuteReader();
             reader.Read();
-            MessageBox.Show("Detalhes do Produto:\n" +
-                "\n" + reader.GetName(1) + ": " + reader.GetString(1) +
-                "\n" + reader.GetName(2) + ": " + reader.GetString(2) +
-                "\n" + reader.GetName(3) + ": " + reader.GetString(3) +
-                "\n" + reader.GetName(4) + ": " + reader.GetInt32(4) +
-                "\n" + reader.GetName(5) + ": R$" + reader.GetSqlMoney(5) +
-                "\n" + reader.GetName(6) + ": " + reader.GetInt16(6) +
-                "\nDimensões: " + reader.GetByte(7) + "m X " + reader.GetByte(8) + "m X " + reader.GetByte(9) +
-                "m\n" + reader.GetName(10) + ": " + reader.GetDecimal(10) +
-                "%\nClassificação Fiscal ou NCM: " + reader.GetInt32(11));
+            string mensagem = "Detalhes do Produto:\n" +
+                "\n" + reader.GetName(0) + ": " + reader.GetInt32(0).ToString() +
+                "\n" + reader.GetName(1) + ": " + reader.GetString(1) ;
+            for (int i = 2; i <= 34; i++)
+            {
+                mensagem += "\n" + reader.GetName(i) + ": " + reader.GetDouble(i).ToString();
+            }
+            MessageBox.Show(mensagem);
             conexaoDB.Close();
         }
 
