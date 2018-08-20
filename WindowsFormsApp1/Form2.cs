c2054987b8ba3d88dcf00a99254bd9667655fdfa
Rafael Alves <ralphawk@gmail.com>
2018-08-20 13:44:52 -0300

    Correção na inserção de dados

--- WindowsFormsApp1/Form2.cs
+++ WindowsFormsApp1/Form2.cs
@@ -8,7 +8,8 @@ namespace WindowsFormsApp1
     public partial class Form2 : Form
     {
         static SqlConnection conexaoDB = new SqlConnection(ConfigurationManager.ConnectionStrings["registro_produtosConnectionString"].ConnectionString);
-        SqlCommand cmdDB = new SqlCommand("insert into Produtos (Nome, Tipo, Especificacao, Quantidade, Preco, Peso, Comprimento, Largura, Altura, TaxaCambio, NCM)", conexaoDB);
+        SqlCommand cmdDB = new SqlCommand("insert into Produtos(Nome, Tipo, Especificacao, Quantidade, Preco, Peso, Comprimento, Largura, Altura, TaxaCambio, NCM)" +
+            "values(@Nome, @Tipo, @Especificacao, @Quantidade, @Preco, @Peso, @Comprimento, @Largura, @Altura, @TaxaCambio, @NCM)", conexaoDB);
         SqlDataReader reader;
 
         public Form2()
@@ -24,18 +25,17 @@ namespace WindowsFormsApp1
         private void Enviar_Click(object sender, EventArgs e)
         {
             conexaoDB.Open();
-            cmdDB.CommandText += " values (" +
-                Nome.Text + ", " +
-                Tipo.Text + ", " +
-                Especificacao.Text + ", " +
-                Qtd.Value + ", " +
-                Preco.Value + ", " +
-                Peso.Value + ", " +
-                Comprimento.Value + ", " +
-                Largura.Value + ", " +
-                Altura.Value + ", " +
-                TaxaCambio.Value + ", " +
-                NCM.Value + ")";
+            cmdDB.Parameters.AddWithValue("@Nome", Nome.Text);
+            cmdDB.Parameters.AddWithValue("@Tipo", Tipo.Text);
+            cmdDB.Parameters.AddWithValue("@Especificacao", Especificacao.Text);
+            cmdDB.Parameters.AddWithValue("@Quantidade", Qtd.Value);
+            cmdDB.Parameters.AddWithValue("@Preco", Preco.Value);
+            cmdDB.Parameters.AddWithValue("@Peso", Peso.Value);
+            cmdDB.Parameters.AddWithValue("@Comprimento", Comprimento.Value);
+            cmdDB.Parameters.AddWithValue("@Largura", Largura.Value);
+            cmdDB.Parameters.AddWithValue("@Altura", Altura.Value);
+            cmdDB.Parameters.AddWithValue("@TaxaCambio", TaxaCambio.Value);
+            cmdDB.Parameters.AddWithValue("@NCM", NCM.Value);
             int linhas = cmdDB.ExecuteNonQuery();
             conexaoDB.Close();
             Application.Restart();
